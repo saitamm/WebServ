0a1e40f4fcd6338628d90ed0c36778c23754429e
sait-amm <sait-amm@c4r3p3.1337.ma>
2025-06-20 12:51:19 +0100

    Request

--- Request.cpp
+++ Request.cpp
@@ -11,14 +11,45 @@ Request::~Request() {}
 
 // }
 
-void ParseRequest(string buffer, Request &request)
+// void ParseRequest(string buffer, Request &request)
+// {
+//     std::cout << "----this is the Request ------\n"
+//               << buffer << endl;
+//     cout << request.getMethod() << endl;
+//     request.setMethod(buffer.substr(0, buffer.find(' ' , 0)));
+//     cout << " -----"<< request.getMethod() << "---"<< endl;
+//     int pos = buffer.find(' ', 0);
+//     pos++;
+//     request.setPath(buffer.substr(pos, buffer.find(' ', pos+1)-pos));
+//     cout << " -----"<< request.getPath()<<"---"<< endl;
+//     pos = buffer.find(' ' , buffer.find('\n', pos));
+//     pos++;
+//     request.setHost(buffer.substr(pos, buffer.find(':', pos+1)-pos));
+//     cout << "------" << request.getHost() << "------" << endl;
+// }
+
+void ParseRequest(int clientSocket, Request &req)
 {
-    std::cout << "----this is the Request ------\n"
-              << buffer << endl;
-    cout << request.getMethod() << endl;
-    // int i = 0;
-    // string line;
-    // while ()
+    std::string Header;
+    char buf[10];
+    ssize_t bytesRead;
+
+    while ((bytesRead = recv(clientSocket, buf, sizeof(buf), 0)) > 0)
+    {
+        Header.append(buf, bytesRead);
+        cout << Header << endl;
+        cout << "-------------------------------------------\n";
+        if (Header.find("Content-Type:") != std::string::npos)
+            break;
+    }
+    //parse header
+    req.setMethod(Header.substr(0, Header.find(' ', 0)));
+    Header.erase(Header.find(' ', 0)+1);
+    std::cout << "---" << req.getMethod() << "------"<< endl;
+    cout << "==="<< Header << endl;
+
+
+    req.setContentLength(8);
 }
 
 // setters
@@ -37,4 +68,4 @@ void Request::setMethod(string meth) { _method = meth; }
 void Request::setPath(string path) { _path = path; }
 void Request::setPort(string port) { _port = port; }
 void Request::setVServer(int val) { _Vserver = val; }
-void Request::setContentLength  (size_t Content) { ContentLength = Content; }
+void Request::setContentLength(size_t Content) { ContentLength = Content; }
