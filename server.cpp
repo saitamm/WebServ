48140bd8d8075e3480eeb084e3cffaa6b616ca1d
Keltoum Nacer <knacer@c3r9p6.1337.ma>
2025-07-21 13:08:42 +0100

    working on multiplixing

--- server.cpp
+++ server.cpp
@@ -77,15 +77,19 @@ int main(int ac, char **av)
                return(printErr("Socket creation failed!"));
           setNonBlocking(serverSocket);
           int opt = 1;
-          setSockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-          sockaddr_in serverAddr{};
+          setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+          sockaddr_in serverAddr;
+          memset(&serverAddr, 0, sizeof(serverAddr));
           serverAddr.sin_family = AF_INET;
           serverAddr.sin_addr.s_addr = INADDR_ANY;
-          bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+          serverAddr.sin_port = htons(servers->at(0).getPort());
+          if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+               return(printErr("bind failed, maybe port is busy!"));
           listen(serverSocket, SOMAXCONN);
           
           int epollFd = epoll_create1(0);
-          epoll_event event{};
+          epoll_event event;
+          memset(&event, 0, sizeof(event));
           event.data.fd = serverSocket;
           event.events = EPOLLIN;
           epoll_ctl(epollFd, EPOLL_CTL_ADD, serverSocket, &event);
@@ -101,59 +105,48 @@ int main(int ac, char **av)
                     {
                          int clientSocket = accept(serverSocket, NULL, NULL);
                          setNonBlocking(clientSocket);
-                         epoll_event clientEvent{};
+                         epoll_event clientEvent;
+                         memset(&clientEvent, 0, sizeof(clientEvent));
                          clientEvent.data.fd = clientSocket;
-                         clientEvent.events = EPOLIN | EPOLLET;
+                         clientEvent.events = EPOLLIN | EPOLLET;
                          epoll_ctl(epollFd, EPOLL_CTL_ADD, clientSocket, &clientEvent);
-                         cout << " New client connected: "<<< clientSocket << endl;
+                         cout << " New client connected: "<< clientSocket << endl;
                     }
                     else
                     {
-                         char buffer[4096];
-                         int bytes = recv(events[i].data.fd, buffer, sizeof(buffer), 0);
-                         if (bytes <= 0)
+                         Request req;
+                         Response resp;
+                         try
                          {
-                              cout << "client disconnected: " << events[i].data.fd<< endl;
-                              close(events[i].data.fd);
+                              resp.setError(200, "OK");
+                              resp.setError(204, "No Content");
+                              resp.setError(403, "Forbidden");
+                              resp.setError(404, "Not Found");
+                              resp.setError(405, "Method Not Allowed");
+                              resp.setError(409, "Conflict");
+                              req.ParseRequest(events[i].data.fd, servers->at(0));
+                              MakeResponce(req, resp);
                          }
-                         else
+                         catch (const std::exception &e)
                          {
-                              cout << "Received: " << string(buffer, bytes) << endl;
-                              send(events[i].data.fd, buffer, bytes, 0);
-                               Request req;
-                              Response resp;
-                              try
+                              cout << "---------------\n";
+                              resp.setStatus(400);
+                              map<int, string> body = servers->at(0).getError_page();
+                              if (body[400][0] == '/')
+                                   body[400].erase(0, 1);
+                              fstream file(body[400].c_str());
+                              if (!file.is_open())
                               {
-                                   resp.setError(200, "OK");
-                                   resp.setError(204, "No Content");
-                                   resp.setError(403, "Forbidden");
-                                   resp.setError(404, "Not Found");
-                                   resp.setError(405, "Method Not Allowed");
-                                   resp.setError(409, "Conflict");
-                                   req.ParseRequest(events[i].data.fd, servers->at(0));
-                                   MakeResponce(req, resp);
+                                   std::cerr << "❌ Failed to open file: " << body[400] << std::endl;
                               }
-                              catch (const std::exception &e)
-                              {
-                                   cout << "---------------\n";
-                                   resp.setStatus(400);
-                                   map<int, string> body = servers->at(0).getError_page();
-                                   if (body[400][0] == '/')
-                                        body[400].erase(0, 1);
-                                   fstream file(body[400].c_str());
-                                   if (!file.is_open())
-                                   {
-                                        std::cerr << "❌ Failed to open file: " << body[400] << std::endl;
-                                   }
-                                   std::string buffer((std::istreambuf_iterator<char>(file)),
-                                                  std::istreambuf_iterator<char>());
-                                   resp.setBody(buffer);
-                                   
-                              }
-                              SendResponse(resp, events[i].data.fd);
-                              close(events[i].data.fd);
+                              std::string buffer((std::istreambuf_iterator<char>(file)),
+                                             std::istreambuf_iterator<char>());
+                              resp.setBody(buffer);
                               
                          }
+                         SendResponse(resp, events[i].data.fd);
+                         close(events[i].data.fd);  
+                         
                     }
                }
           }
@@ -166,79 +159,3 @@ int main(int ac, char **av)
 
 }
 
-// int main(int ac, char **av)
-// {
-//      if (ac != 2)
-//      {
-//           cout << "Error: ./Webserv <file.conf> \n";
-//           return 1;
-//      }
-//      ConfigFile config;
-//      vector<ConfigFile> *servers;
-//      try
-//      {
-
-//           servers = config.ParseConfigFile(av[1]);
-//           // cout << "--------------------Server-----------------\n";
-//           int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-//           if (serverSocket == -1)
-//           {
-//                std::cerr << "Socket creation failed!\n";
-//                return 1;
-//           }
-//           sockaddr_in serverAddr;
-//           serverAddr.sin_family = AF_INET;
-//           serverAddr.sin_port = htons(servers->at(0).getPort());
-//           serverAddr.sin_addr.s_addr = INADDR_ANY;
-//           int opt = 1;
-//           setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-//           if (bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-//           {
-//                std::cerr << "Bind failed! Maybe port is busy.\n";
-//                return 1;
-//           }
-//           listen(serverSocket, 5);
-//           int clientSocket;
-//           clientSocket = accept(serverSocket, NULL, NULL);
-//           cout << "--------------------Request-----------------\n\n";
-//           Request req;
-//           Response resp;
-//           try
-//           {
-//                resp.setError(200, "OK");
-//                resp.setError(204, "No Content");
-//                resp.setError(403, "Forbidden");
-//                resp.setError(404, "Not Found");
-//                resp.setError(405, "Method Not Allowed");
-//                resp.setError(409, "Conflict");
-//                req.ParseRequest(clientSocket, servers->at(0));
-//                MakeResponce(req, resp);
-//           }
-//           catch (const std::exception &e)
-//           {
-//                cout << "---------------\n";
-//                resp.setStatus(400);
-//                map<int, string> body = servers->at(0).getError_page();
-//                if (body[400][0] == '/')
-//                     body[400].erase(0, 1);
-//                fstream file(body[400].c_str());
-//                if (!file.is_open())
-//                {
-//                     std::cerr << "❌ Failed to open file: " << body[400] << std::endl;
-//                }
-//                std::string buffer((std::istreambuf_iterator<char>(file)),
-//                                   std::istreambuf_iterator<char>());
-//                resp.setBody(buffer);
-               
-//           }
-//           SendResponse(resp, clientSocket);
-//           close(clientSocket);
-//           close(serverSocket);
-//           // close(clientSocket);
-//      }
-//      catch (exception &e)
-//      {
-//           cout << e.what() << endl;
-//      }
-//      return 0;
-// }
