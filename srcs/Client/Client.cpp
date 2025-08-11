94346908bdf7aa498243ab97e140a5545cc44800
sait-amm <sait-amm@c3r9p6.1337.ma>
2025-08-11 09:37:38 +0100

    my version in chunked body

--- srcs/Client/Client.cpp
+++ srcs/Client/Client.cpp
@@ -97,7 +97,6 @@ void Client::buildResponse(int clientSocket)
             string f = ll.str() + "." + type;
             string Up = _resp->getRequest()->getConfigFile().getRoot() + "/" + _resp->getRequest()->getLocation()->getUp_store() + "/" + f;
             _resp->getFile().open(Up.c_str(), std::ios::out | std::ios::trunc | std::ios::binary);
-            cout << "this is my File =  " << Up<<endl;
             if (!_resp->getFile().is_open())
             {
                 cerr << "soummaya\n";
